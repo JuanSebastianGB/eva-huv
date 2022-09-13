822cb1a4c5583812fd973180b2a9e470e4378ee9
JuanGonzaGB <3393@holbertonstudents.com>
2022-09-13 13:28:06 +0000

    Adding Tickets table with basic information

---
+++ server/migrations/20220913125904-create-final-disposition.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('finalDispositions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      finalDispositionDate: {
+        type: Sequelize.DATE
+      },
+      file: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('finalDispositions');
+  }
+};
---
+++ server/migrations/20220913125908-create-device.js
@@ -0,0 +1,90 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Devices', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      image: {
+        type: Sequelize.STRING
+      },
+      code: {
+        type: Sequelize.STRING
+      },
+      oldCode: {
+        type: Sequelize.STRING
+      },
+      model: {
+        type: Sequelize.STRING
+      },
+      serial: {
+        type: Sequelize.STRING
+      },
+      cost: {
+        type: Sequelize.INTEGER
+      },
+      location: {
+        type: Sequelize.STRING
+      },
+      acquisitionDate: {
+        type: Sequelize.DATE
+      },
+      installationDate: {
+        type: Sequelize.DATE
+      },
+      warehouseReceiptDate: {
+        type: Sequelize.DATE
+      },
+      manufactoringDate: {
+        type: Sequelize.DATE
+      },
+      status: {
+        type: Sequelize.INTEGER
+      },
+      serviceId: {
+        type: Sequelize.INTEGER
+      },
+      areaId: {
+        type: Sequelize.STRING
+      },
+      manualId: {
+        type: Sequelize.INTEGER
+      },
+      guideId: {
+        type: Sequelize.INTEGER
+      },
+      ownerId: {
+        type: Sequelize.INTEGER
+      },
+      finalDispositionId: {
+        type: Sequelize.INTEGER
+      },
+      riskTypeId: {
+        type: Sequelize.INTEGER
+      },
+      biomedicalClassificationId: {
+        type: Sequelize.INTEGER
+      },
+      deviceStatusId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Devices');
+  }
+};
