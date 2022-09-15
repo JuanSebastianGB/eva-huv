a804e7d4854141b314b5f406feb34fb3fd4fb8cb
JuanGonzaGB <3393@holbertonstudents.com>
2022-09-15 14:27:00 +0000

    Adding a route to gennerate a random number, storing it and recovering it form redis

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,6 +7,7 @@ const router = express.Router();
 
 router.get('/', (req, res) => res.json({ input: 'input point' }));
 router.get('/status', AppController.getStatus);
+router.get('/random', AppController.randomNumber);
 
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
