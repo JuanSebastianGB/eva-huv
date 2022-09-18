0bfe738adc28b37a6cd2b0a5c6f109f63546cc02
JuanGonzaGB <3393@holbertonstudents.com>
2022-09-18 20:37:45 +0000

    Adding function to count all the respective tables

--- server/README.md
+++ server/README.md
@@ -3,3 +3,14 @@
 ```sh
 npx sequelize-cli db:seed:all
 ```
+
+## HTTP CODES:
+
+```
+- 200 OK
+- 201 CREATED
+- 202 ACCEPTED
+
+- 400 BAD REQUEST
+- 401 UNAUTHORIZED
+```
--- server/test/area.test.js
+++ server/test/area.test.js
@@ -1,4 +1,10 @@
-const { expect } = require('chai');
+/* eslint-disable jest/prefer-expect-assertions */
+/* eslint-disable jest/expect-expect */
+/* eslint-disable jest/no-test-callback */
+/* eslint-disable no-unused-vars */
+/* eslint-disable jest/no-hooks */
+
+const chai = require('chai');
 const request = require('request');
 require('dotenv').config();
 
@@ -8,7 +14,7 @@ const URL = `http://localhost:${PORT}`;
 let headers;
 let method;
 
-describe('Test suit for areas endpoint', () => {
+describe('test suit for areas endpoint', () => {
   beforeEach(() => {
     headers = {
       Accept: 'application/json',
@@ -17,50 +23,50 @@ describe('Test suit for areas endpoint', () => {
     method = 'GET';
   });
 
-  it('Get /areas must return status code 200', (done) => {
-    const url = `${URL}/areas`,
-      options = { url, method, headers };
+  it('get /areas must return status code 200', (done) => {
+    const url = `${URL}/areas`;
+    const options = { url, method, headers };
     request(options, (err, res, body) => {
-      expect(res.statusCode).to.equal(200);
+      chai.expect(res.statusCode).to.equal(200);
       done();
     });
   });
-  it('Get /areas must return a list', (done) => {
-    const url = `${URL}/areas`,
-      options = { url, method, headers };
+  it('get /areas must return a list', (done) => {
+    const url = `${URL}/areas`;
+    const options = { url, method, headers };
     request(options, (err, res, body) => {
       const jsonObject = JSON.parse(body);
-      expect(Array.isArray(jsonObject)).to.equal(true);
+      chai.expect(Array.isArray(jsonObject)).to.equal(true);
       done();
     });
   });
-  it('Get /areas/:id must return a json object with "area" key and an object value', (done) => {
-    const url = `${URL}/areas/1`,
-      options = { url, method, headers };
+  it('get /areas/:id must return a json object with "area" key and an object value', (done) => {
+    const url = `${URL}/areas/1`;
+    const options = { url, method, headers };
     request(options, (err, res, body) => {
       const jsonObject = JSON.parse(body);
-      expect(Object.keys(jsonObject).includes('area')).to.equal(true);
-      expect(typeof jsonObject['area']).to.equal('object');
+      chai.expect(Object.keys(jsonObject).includes('area')).to.equal(true);
+      chai.expect(typeof jsonObject.area).to.equal('object');
       done();
     });
   });
-  it('Get /areas/:id should return an error if id is not valid', (done) => {
-    const url = `${URL}/areas/hello`,
-      options = { url, method, headers };
+  it('get /areas/:id should return an error if id is not valid', (done) => {
+    const url = `${URL}/areas/hello`;
+    const options = { url, method, headers };
     request(options, (err, res, body) => {
       const jsonObject = JSON.parse(body);
-      expect(typeof jsonObject).to.equal('object');
-      expect(Object.keys(jsonObject).length).to.equal(1);
-      expect(jsonObject['err']).to.equal('id must be a number');
+      chai.expect(typeof jsonObject).to.equal('object');
+      chai.expect(Object.keys(jsonObject).length).to.equal(1);
+      chai.expect(jsonObject.err).to.equal('id must be a number');
       done();
     });
   });
-  it('Get /areas/:id most give an empty object when id is not found', (done) => {
-    const url = `${URL}/areas/99999999999999`,
-      options = { url, method, headers };
+  it('get /areas/:id most give an empty object when id is not found', (done) => {
+    const url = `${URL}/areas/99999999999999`;
+    const options = { url, method, headers };
     request(options, (err, res, body) => {
       const jsonObject = JSON.parse(body);
-      expect(Object.entries(jsonObject).length).to.equal(0);
+      chai.expect(Object.entries(jsonObject).length).to.equal(0);
       done();
     });
   });
