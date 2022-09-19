195f49ba934d6cd516578af44effa68ffc6833a9
JuanGonzaGB <3393@holbertonstudents.com>
2022-09-19 14:54:30 +0000

    Making 2 first test using async function

--- server/test/area.test.js
+++ server/test/area.test.js
@@ -4,70 +4,70 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable jest/no-hooks */
 
-const chai = require('chai');
-const request = require('request');
-require('dotenv').config();
+// const chai = require('chai');
+// const request = require('request');
+// require('dotenv').config();
 
-const PORT = process.env.PORT || 5000;
-const URL = `http://localhost:${PORT}`;
+// const PORT = process.env.PORT || 5000;
+// const URL = `http://localhost:${PORT}`;
 
-let headers;
-let method;
+// let headers;
+// let method;
 
-describe('test suit for areas endpoint', () => {
-  beforeEach(() => {
-    headers = {
-      Accept: 'application/json',
-      skip: true,
-    };
-    method = 'GET';
-  });
+// describe('test suit for areas endpoint', () => {
+//   beforeEach(() => {
+//     headers = {
+//       Accept: 'application/json',
+//       skip: true,
+//     };
+//     method = 'GET';
+//   });
 
-  it('get /areas must return status code 200', (done) => {
-    const url = `${URL}/areas`;
-    const options = { url, method, headers };
-    request(options, (err, res, body) => {
-      chai.expect(res.statusCode).to.equal(200);
-      done();
-    });
-  });
-  it('get /areas must return a list', (done) => {
-    const url = `${URL}/areas`;
-    const options = { url, method, headers };
-    request(options, (err, res, body) => {
-      const jsonObject = JSON.parse(body);
-      chai.expect(Array.isArray(jsonObject)).to.equal(true);
-      done();
-    });
-  });
-  it('get /areas/:id must return a json object with "area" key and an object value', (done) => {
-    const url = `${URL}/areas/1`;
-    const options = { url, method, headers };
-    request(options, (err, res, body) => {
-      const jsonObject = JSON.parse(body);
-      chai.expect(Object.keys(jsonObject).includes('area')).to.equal(true);
-      chai.expect(typeof jsonObject.area).to.equal('object');
-      done();
-    });
-  });
-  it('get /areas/:id should return an error if id is not valid', (done) => {
-    const url = `${URL}/areas/hello`;
-    const options = { url, method, headers };
-    request(options, (err, res, body) => {
-      const jsonObject = JSON.parse(body);
-      chai.expect(typeof jsonObject).to.equal('object');
-      chai.expect(Object.keys(jsonObject).length).to.equal(1);
-      chai.expect(jsonObject.err).to.equal('id must be a number');
-      done();
-    });
-  });
-  it('get /areas/:id most give an empty object when id is not found', (done) => {
-    const url = `${URL}/areas/99999999999999`;
-    const options = { url, method, headers };
-    request(options, (err, res, body) => {
-      const jsonObject = JSON.parse(body);
-      chai.expect(Object.entries(jsonObject).length).to.equal(0);
-      done();
-    });
-  });
-});
+//   it('get /areas must return status code 200', (done) => {
+//     const url = `${URL}/areas`;
+//     const options = { url, method, headers };
+//     request(options, (err, res, body) => {
+//       chai.expect(res.statusCode).to.equal(200);
+//       done();
+//     });
+//   });
+//   it('get /areas must return a list', (done) => {
+//     const url = `${URL}/areas`;
+//     const options = { url, method, headers };
+//     request(options, (err, res, body) => {
+//       const jsonObject = JSON.parse(body);
+//       chai.expect(Array.isArray(jsonObject)).to.equal(true);
+//       done();
+//     });
+//   });
+//   it('get /areas/:id must return a json object with "area" key and an object value', (done) => {
+//     const url = `${URL}/areas/1`;
+//     const options = { url, method, headers };
+//     request(options, (err, res, body) => {
+//       const jsonObject = JSON.parse(body);
+//       chai.expect(Object.keys(jsonObject).includes('area')).to.equal(true);
+//       chai.expect(typeof jsonObject.area).to.equal('object');
+//       done();
+//     });
+//   });
+//   it('get /areas/:id should return an error if id is not valid', (done) => {
+//     const url = `${URL}/areas/hello`;
+//     const options = { url, method, headers };
+//     request(options, (err, res, body) => {
+//       const jsonObject = JSON.parse(body);
+//       chai.expect(typeof jsonObject).to.equal('object');
+//       chai.expect(Object.keys(jsonObject).length).to.equal(1);
+//       chai.expect(jsonObject.err).to.equal('id must be a number');
+//       done();
+//     });
+//   });
+//   it('get /areas/:id most give an empty object when id is not found', (done) => {
+//     const url = `${URL}/areas/99999999999999`;
+//     const options = { url, method, headers };
+//     request(options, (err, res, body) => {
+//       const jsonObject = JSON.parse(body);
+//       chai.expect(Object.entries(jsonObject).length).to.equal(0);
+//       done();
+//     });
+//   });
+// });
--- server/test/service.test.js
+++ server/test/service.test.js
@@ -1,60 +1,47 @@
 const chai = require('chai');
 const request = require('supertest');
 const app = require('../server.js');
-let { Service } = require('../models');
+const { expect } = chai;
+// let { Service } = require('../models');
 
 // import app from '../server';
-const temporal = [];
 describe('API', () => {
-  // beforeEach((done) => {
-  //   // temporal.push('hi');
-  //   // console.log({ msj: 'beforeEach' });
-  //   // User.destroy();
-  //   // done();
-  // });
-  describe('get /services', () => {
-    it('returns the services', (done) => {
-      request(app)
+  describe('get /services Get all', async () => {
+    it('returns the services', async () => {
+      const { body, status } = await request(app)
         .get('/services')
-        .set('skip', true)
-        .expect('Content-Type', /json/)
-        .expect(200)
-        .end(function (err, res) {
-          // console.log(res._body);
-          if (err) throw err;
-          return done();
-        });
+        .set('skip', true);
+      const { data } = body;
+      expect(status).to.equal(200);
+      expect(Array.isArray(body)).to.equal(true);
     });
   });
-  describe('get /services/:id', () => {
-    it('returns the services', (done) => {
-      request(app)
-        .get('/services/1')
-        .set('skip', true)
-        .expect('Content-Type', /json/)
-        .expect(200)
-        .end(function (err, res) {
-          // console.log(res._body);
-          if (err) throw err;
-        });
-      done();
+  describe('get /services Get By Id', async () => {
+    it('returns the services', async () => {
+      const { body, status } = await request(app)
+        .get('/services/id')
+        .set('skip', true);
+      const { data } = body;
+      expect(status).to.equal(200);
+      expect(typeof body).to.equal('object');
     });
   });
-  describe('delete /services/1', () => {
-    it('delete a service By Id', (done) => {
-      request(app)
-        .delete('/services/1')
-        .set('skip', true)
-        .expect('Content-Type', '/json/')
-        .expect(200)
-        .end((err, res) => {
-          const { text } = res;
-          const response = JSON.parse(text);
-          console.log({ response });
-          done();
-        });
-    });
-  });
+
+  // describe('delete /services/1', () => {
+  //   it('delete a service By Id', (done) => {
+  //     request(app)
+  //       .delete('/services/1')
+  //       .set('skip', true)
+  //       .expect('Content-Type', '/json/')
+  //       .expect(200)
+  //       .end((err, res) => {
+  //         const { text } = res;
+  //         const response = JSON.parse(text);
+  //         console.log({ response });
+  //         done();
+  //       });
+  //   });
+  // });
   // describe('post /services', () => {
   //   it('create a new service', (done) => {
   //     Service.destroy({ where: { name: 'New service' } });
