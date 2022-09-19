e736f1c32d9863c6e06837d4028e3ced89ee6f12
JuanGonzaGB <3393@holbertonstudents.com>
2022-09-19 13:45:54 +0000

    Adding first supertest implementation by deleting a service and after creating it to check if this exists

--- server/test/service.test.js
+++ server/test/service.test.js
@@ -1,10 +1,17 @@
 const chai = require('chai');
 const request = require('supertest');
 const app = require('../server.js');
+let { Service } = require('../models');
 
 // import app from '../server';
-
+const temporal = [];
 describe('API', () => {
+  // beforeEach((done) => {
+  //   // temporal.push('hi');
+  //   // console.log({ msj: 'beforeEach' });
+  //   // User.destroy();
+  //   // done();
+  // });
   describe('get /services', () => {
     it('returns the services', (done) => {
       request(app)
@@ -13,10 +20,39 @@ describe('API', () => {
         .expect('Content-Type', /json/)
         .expect(200)
         .end(function (err, res) {
-          console.log(res._body);
+          // console.log(res._body);
           if (err) throw err;
           return done();
         });
+    });
+  });
+  describe('get /services/:id', () => {
+    it('returns the services', (done) => {
+      request(app)
+        .get('/services/1')
+        .set('skip', true)
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .end(function (err, res) {
+          // console.log(res._body);
+          if (err) throw err;
+        });
+      done();
+    });
+  });
+  describe('post /services', () => {
+    it('create a new service', (done) => {
+      Service.destroy({ where: { name: 'New service' } });
+      request(app)
+        .post('/services')
+        .send({ name: 'New service' })
+        .set('skip', true)
+        .expect('Content-Type', /json/)
+        .expect(201)
+        .end((err, res) => {
+          if (err) throw err;
+        });
+      return done();
     });
   });
 });
