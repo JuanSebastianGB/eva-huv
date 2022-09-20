369f151ef04fed92210aba46e9906e706d45a47f
JuanGonzaGB <3393@holbertonstudents.com>
2022-09-20 20:07:17 +0000

    Adding test for create, delete, and gets service methods

--- server/test/service.test.js
+++ server/test/service.test.js
@@ -1,12 +1,28 @@
+/* eslint-disable jest/lowercase-name */
+/* eslint-disable jest/valid-describe */
+/* eslint-disable jest/no-commented-out-tests */
+/* eslint-disable jest/valid-expect */
+/* eslint-disable jest/prefer-expect-assertions */
+/* eslint-disable jest/expect-expect */
+/* eslint-disable jest/no-test-callback */
+/* eslint-disable no-unused-vars */
+/* eslint-disable jest/no-hooks */
+
 const chai = require('chai');
 const request = require('supertest');
-const app = require('../server.js');
-const { expect } = chai;
-// let { Service } = require('../models');
+const app = require('../server');
 
-// import app from '../server';
+const { expect } = chai;
+const { Area, Service } = require('../models');
+
+const truncateModels = async () => {
+  await Area.destroy({ where: {}, force: true });
+  await Service.destroy({ where: {}, force: true });
+};
+
 describe('API', () => {
-  describe('get /services Get all', async () => {
+  beforeEach(async () => truncateModels());
+  describe('Get /services Get all', async () => {
     it('returns the services', async () => {
       const { body, status } = await request(app)
         .get('/services')
@@ -16,7 +32,7 @@ describe('API', () => {
       expect(Array.isArray(body)).to.equal(true);
     });
   });
-  describe('get /services Get By Id', async () => {
+  describe('Get /services Get By Id', async () => {
     it('returns the services', async () => {
       const { body, status } = await request(app)
         .get('/services/id')
@@ -26,35 +42,38 @@ describe('API', () => {
       expect(typeof body).to.equal('object');
     });
   });
-
-  // describe('delete /services/1', () => {
-  //   it('delete a service By Id', (done) => {
-  //     request(app)
-  //       .delete('/services/1')
-  //       .set('skip', true)
-  //       .expect('Content-Type', '/json/')
-  //       .expect(200)
-  //       .end((err, res) => {
-  //         const { text } = res;
-  //         const response = JSON.parse(text);
-  //         console.log({ response });
-  //         done();
-  //       });
-  //   });
-  // });
-  // describe('post /services', () => {
-  //   it('create a new service', (done) => {
-  //     Service.destroy({ where: { name: 'New service' } });
-  //     request(app)
-  //       .post('/services')
-  //       .send({ name: 'New service' })
-  //       .set('skip', true)
-  //       .expect('Content-Type', /json/)
-  //       .expect(201)
-  //       .end((err, res) => {
-  //         if (err) throw err;
-  //       });
-  //     return done();
-  //   });
-  // });
+  describe('Post /services', async () => {
+    it('create a new service called test service', async () => {
+      const { body, status } = await request(app)
+        .post('/services')
+        .send({ name: 'test service' })
+        .set('skip', true);
+      const { response } = body;
+      const { id } = response;
+      await expect(status).to.equal(201);
+      await expect(typeof body).to.equal('object');
+    });
+  });
+  describe('Delete /services/:id', () => {
+    it('Delete an specific service successfully', async () => {
+      const { body, status } = await request(app)
+        .post('/services')
+        .send({ name: 'test service' })
+        .set('skip', true);
+      const { response } = body;
+      const { id } = response;
+      const { body: bodyDelete } = await request(app)
+        .delete(`/services/${id}`)
+        .set('skip', true);
+      const { response: responseDelete } = bodyDelete;
+      await expect(responseDelete).to.equal(1);
+    });
+    it("Delete an specific service when this doesn't exists", async () => {
+      const { body } = await request(app)
+        .delete('/services/1')
+        .set('skip', true);
+      const { err } = body;
+      await expect(err).to.equal('Not Found');
+    });
+  });
 });
